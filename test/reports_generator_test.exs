c37b714d9d8b3a9d6d991ab38fbe29f5fbf026fb
Francine Guimarães <francinesguimaraes@outlook.com>
2021-07-19 02:23:25 -0300

    feat: add tests

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -2,7 +2,29 @@ defmodule ReportsGeneratorTest do
   use ExUnit.Case
   doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  test "should sum each item per id" do
+    file = "report_test.csv"
+
+    expected_result = %{
+      "foods" => %{
+        "açaí" => 1,
+        "churrasco" => 2,
+        "esfirra" => 3,
+        "hambúrguer" => 3,
+        "pizza" => 2
+      },
+      "sum_items" => %{
+        "1" => 68,
+        "2" => 45,
+        "3" => 73,
+        "4" => 49,
+        "5" => 18,
+        "6" => 52,
+        "7" => 24,
+        "8" => 36
+      }
+    }
+
+    assert ReportsGenerator.reports_generator(file) == expected_result
   end
 end
