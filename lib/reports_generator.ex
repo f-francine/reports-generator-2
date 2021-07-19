f9bf88dd8cc69cbbbf3381b53f963ce5caad27a7
Francine Guimarães <francinesguimaraes@outlook.com>
2021-07-19 02:24:48 -0300

    feat: add food frequency report

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,18 +4,26 @@ defmodule ReportsGenerator do
   def reports_generator(file) do
     file
     |> Parser.parse_file()
-    |> Enum.reduce(%{}, fn [id, _food, price], report ->
-      IO.inspect(report)
-
-      sum_items(report, id, price)
+    |> Enum.reduce(%{"foods" => %{}, "sum_items" => %{}}, fn [id, food, price], report ->
+      food_frequency = food_frequency(report["foods"], food)
+      sum_items = sum_items_per_id(report["sum_items"], id, price)
+      %{report | "foods" => food_frequency, "sum_items" => sum_items}
     end)
   end
 
-  defp sum_items(report, id, price) do
-    if Map.has_key?(report, id) do
-      Map.put(report, id, report[id] + price)
+  defp sum_items_per_id(sum_items, id, price) do
+    if Map.has_key?(sum_items, id) do
+      Map.put(sum_items, id, sum_items[id] + price)
     else
-      Map.put(report, id, price)
+      Map.put(sum_items, id, price)
+    end
+  end
+
+  defp food_frequency(foods, food) do
+    if Map.has_key?(foods, food) do
+      Map.put(foods, food, foods[food] + 1)
+    else
+      Map.put(foods, food, 1)
     end
   end
 end
